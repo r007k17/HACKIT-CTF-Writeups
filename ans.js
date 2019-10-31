232208218902cc032b0a30365c0c182309f0f054
r007k17 <karthikaryabhat@gmail.com>
2019-10-31 18:24:40 +0530

    Update ans.js

--- ans.js
+++ ans.js
@@ -4,4 +4,4 @@
 */
 
 document.write ("This is remote text via xss.js located at xss.rocks " + document.cookie);
-alert ("This is remote text via xss.js located at xss.rocks " + document.cookie);
+alert ("This is remote text via xss.js located at xss.rocks " + document.cookie); 
